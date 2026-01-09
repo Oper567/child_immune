0d23cbe699ef9e2b8e2f98017e6508a1f23dc006
Legend Franklin <legendfranklin27@gmail.com>
2026-01-09 18:19:12 +0000

    upgrade

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:10000/api/:path*', // Points to your Express port
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
