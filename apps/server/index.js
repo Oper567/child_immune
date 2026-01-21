194634ea462e60a107c17e940f1f9d6b6899b466
Legend Franklin <legendfranklin27@gmail.com>
2026-01-21 05:59:47 +0000

    natt

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,6 +1,6 @@
 ﻿const express = require("express");
 const cors = require("cors");
-const { prisma } = require("@immunize/database");
+const { prisma } = require("../../packages/database");
 const bcrypt = require("bcryptjs");
 
 // Utilities & Middleware
